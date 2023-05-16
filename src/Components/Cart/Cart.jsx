14045c0eb91d0506e7fb20b19c8bbc546c2f6d5a
Abdelrahman Elsayed <abdelrahman.elsayed0122@gmail.com>
2023-05-16 16:51:14 +0300

    Update the Cart page

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,15 +1,19 @@
 import React, { useContext, useEffect, useState } from "react";
 import styles from "./Cart.module.css";
 import { cartContext } from "../../Context/CartContext";
+import { toast } from "react-hot-toast";
 
 export default function Cart() {
-  let { getLoggedUserCart } = useContext(cartContext);
+
+  // bn distract mn el carContext 
+  let { getLoggedUserCart , removeItem, updateProductCount } = useContext(cartContext);
 
   const [cartDetails, setcartDetails] = useState(null)
+
   async function getCart()
    {
     let response = await getLoggedUserCart();
-    console.log(response);
+  //  console.log(response);
     
 
     if (response?.data?.status === "success")
@@ -17,6 +21,25 @@ export default function Cart() {
       setcartDetails(response.data.data);
     }
   }
+
+  //function delete item will call the removeItem from CartContext
+ async function deleteItem(productId)
+  {
+   let response = await  removeItem(productId);
+   // will make he update realtime 
+   //mn el 25er el usestate ele fo2 ht4t8l fa ha render mn 2wl we gded fa el update hekon realtime
+   setcartDetails(response.data.data);
+   toast('item removed succesfully')
+ //  console.log(response);
+  }
+
+  async function updateProductQuantity(productId, count)
+  {
+   let response = await  updateProductCount(productId, count);
+   setcartDetails(response.data.data);
+   toast('product count Updated');
+  }
+
 
   useEffect(() => {
     getCart();
@@ -27,7 +50,7 @@ export default function Cart() {
     cartDetails !== null?  <div className="bg-main-light p-4 my-4">
     <h3>Shop Cart:</h3>
     <h6 className="text-main">Total Cart Price : {cartDetails.totalCartPrice} EGP</h6>
-    {cartDetails.products.map((product)=> <div className="row align-items-center border-bottom py-2">
+    {cartDetails.products.map((product)=> <div key={product.product._id} className="row align-items-center border-bottom py-2">
       <div className="col-md-1">
        <img src={product.product.imageCover} className="w-100" alt=""/> 
       </div>
@@ -36,19 +59,18 @@ export default function Cart() {
         <div>
         <h6>{product.product.title}</h6>
          <h6 className="text-main"> Price: {product.price}</h6>
-         <button className="btn m-0 p-0"><i className="fa-regular text-main fa-trash-can"></i> Remove</button>
+         <button className="btn m-0 p-0" onClick={()=> deleteItem(product.product._id)}><i className="fa-regular text-main fa-trash-can"></i> Remove</button>
         </div>
 
         <div>
-         <button className="btn border-main btn-small">+</button>
+         <button onClick={()=> updateProductQuantity(product.product._id, product.count+1) } className="btn border-main btn-small">+</button>
          <span className="mx-2">{product.count}</span>
-         <button className="btn border-main btn-small">-</button>
+         <button onClick={()=> updateProductQuantity(product.product._id, product.count-1) }  className="btn border-main btn-small">-</button>
         </div>
       
       </div>
     </div>)}
   </div>:null
   }
-<h2>Cart</h2>
   </>
 }
--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -4,6 +4,8 @@ export let cartContext = createContext();
 
 export function CartContextProvider(props) {
   let headers = { token: localStorage.getItem("userToken") };
+
+  // add to cart method {Post}
    function addToCart(x) {
     return axios.post(
       `https://ecommerce.routemisr.com/api/v1/cart`,
@@ -15,6 +17,8 @@ export function CartContextProvider(props) {
       }).then((response)=>response)
         .catch((error)=>error);
   }
+
+  // get items in the cart {get}
   function getLoggedUserCart(x) {
     return axios.get(`https://ecommerce.routemisr.com/api/v1/cart`,
       {
@@ -23,8 +27,31 @@ export function CartContextProvider(props) {
         .catch((error)=>error);
   }
 
+  //Delete item from the cart 
+  function removeItem(productId) {
+    return axios.delete(`https://ecommerce.routemisr.com/api/v1/cart/${productId}`,
+      {
+        headers: headers
+         
+      }).then((response)=>response)
+        .catch((error)=>error);    
+  }
+  //update product count
+  function updateProductCount(productId, count) {
+    return axios.put(`https://ecommerce.routemisr.com/api/v1/cart/${productId}`,
+    {
+      //fe eldatabase fe count hwa heb3at fe el count
+      count:count
+    },
+      {
+        headers: headers
+         
+      }).then((response)=>response)
+        .catch((error)=>error);    
+  }
   return (
-    <cartContext.Provider value={{addToCart, getLoggedUserCart}}>
+    // bn5leha globle 3l project kolo
+    <cartContext.Provider value={{addToCart, getLoggedUserCart, removeItem, updateProductCount}}>
       {props.children}
       </cartContext.Provider>
   );
