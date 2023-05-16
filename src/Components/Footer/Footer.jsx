6d21ea20626021af8b0e834326012e8fb039e80b
Abdelrahman Elsayed <abdelrahman.elsayed0122@gmail.com>
2023-05-16 17:15:00 +0300

    Add the Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,121 @@
-import React from 'react'
-import styles from './Footer.module.css'
-export default function Footer() {
-  return <>
-    
-    <h2>Footer</h2>
-    
-    </>
-  
+import React from 'react';
+import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
+
+export default function App() {
+  return (
+    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+        <div className='me-5 d-none d-lg-block'>
+          <span>Get connected with us on social networks:</span>
+        </div>
+
+        <div>
+          <a href='https://www.facebook.com/profile.php?id=100003799342442' className='me-4 text-reset'>
+            <MDBIcon color='secondary' fab icon='facebook-f' />
+          </a>
+          <a href='https://www.instagram.com/abdelrahman_elsayed123/' className='me-4 text-reset'>
+            <MDBIcon color='secondary' fab icon='instagram' />
+          </a>
+          <a href='https://www.linkedin.com/in/abdelrahman-elsayed-828736234/' className='me-4 text-reset'>
+            <MDBIcon color='secondary' fab icon='linkedin' />
+          </a>
+          <a href='https://github.com/Abdelrahman0122' className='me-4 text-reset'>
+            <MDBIcon color='secondary' fab icon='github' />
+          </a>
+        </div>
+      </section>
+
+      <section className=''>
+        <MDBContainer className='text-center text-md-start mt-5'>
+          <MDBRow className='mt-3'>
+            <MDBCol md='3' lg='4' xl='3' className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>
+                <MDBIcon color='secondary' icon='gem' className='me-3' />
+                E-Commerce-App
+              </h6>
+              <p>
+                Lorem ipsum dolor sit
+                amet, consectetur adipisicing elit.Lorem ipsum dolor sit
+                amet, consectetur adipisicing elit.Lorem ipsum dolor sit
+                amet, consectetur adipisicing elit.
+              </p>
+            </MDBCol>
+
+            <MDBCol md='2' lg='2' xl='2' className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Example
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                Example
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                Example
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                Example
+                </a>
+              </p>
+            </MDBCol>
+
+            <MDBCol md='3' lg='2' xl='2' className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                Example
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                Example
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                Example
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                Example
+                </a>
+              </p>
+            </MDBCol>
+
+            <MDBCol md='4' lg='3' xl='3' className='mx-auto mb-md-0 mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+              <p>
+                <MDBIcon color='secondary' icon='home' className='me-2' />
+                New York, NY 10012, US
+              </p>
+              <p>
+                <MDBIcon color='secondary' icon='envelope' className='me-3' />
+                info@example.com
+              </p>
+              <p>
+                <MDBIcon color='secondary' icon='phone' className='me-3' /> + 01 234 567 88
+              </p>
+              <p>
+                <MDBIcon color='secondary' icon='print' className='me-3' /> + 01 234 567 89
+              </p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
+
+      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+        © 2021 Copyright:
+        <a className='text-reset fw-bold' href='https://github.com/Abdelrahman0122'>
+          Abdelrahman Elsayed
+        </a>
+      </div>
+    </MDBFooter>
+  );
 }
